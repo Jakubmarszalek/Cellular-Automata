2fe9bb473d71a1532f46ef5835865cbdb84d115a
marszaj <JakubMarszalek@autodesk.com>
2019-12-11 22:35:28 +0100

    Grain_Curvature_Influance

--- Celluar-automata-creator.py
+++ Celluar-automata-creator.py
@@ -1,16 +1,12 @@
 import random
 import copy
-import time
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 import argparse
 import io
-from ast import literal_eval
 from tkinter import *
 from tkinter.ttk import *
 from PIL import ImageTk, Image
-import tkinter
 base = 2
 stop_point = base - 1
 
@@ -37,7 +33,7 @@ def parse_arguments():
 
 
 def gui_function():
-    def main(n, custom, random_start, neighbourhood, export, base, inclusion_number, radius_min, radius_max):
+    def main(n, custom, random_start, neighbourhood, export, base, inclusion_number, radius_min, radius_max, proc):
         new = {}
         check_new = []
         Z = list()
@@ -75,7 +71,7 @@ def gui_function():
             while 1:
                 x = random.randint(0, n - 1)
                 y = random.randint(0, n - 1)
-                if board[x][y] == base:  # not in new:
+                if board[x][y] == base:
                     board[x][y] = i
                     break
             new.update({"random{0}".format(i): [x, y, i]})
@@ -90,20 +86,22 @@ def gui_function():
             Z.clear()
             for j in board:
                 Z.extend(j)
-            board = one_grow(board, base, n, check_new, neighbourhood)
+            if neighbourhood == "Grain_Curvature":
+                board = grain_curvature(board, base, n, check_new, int(proc))
+            else:
+                board = one_grow(board, base, n, check_new, neighbourhood)
             new_count = Z.count(base)
             if new_count == old_count:
                 print("Part of image blocking by inculsion")
                 break
             old_count = new_count
-            print(str(100 - new_count * 100 / (n * n))[0:5] + "%")
+            proc_end = str(100 - new_count * 100 / (n * n))[0:5] + "%"
             image = np.zeros(len(Z))
             for i in range(len(image)):
                 image[i] = Z[i]
             image = image.reshape((n, n))
             plt.matshow(image)
-            #plt.savefig('png_file/output{0}.jpg'.format(p))
-            callback(p, plt)
+            callback(proc_end, plt)
             plt.close()
             if not base in Z:
                 break
@@ -122,7 +120,8 @@ def gui_function():
             plt.savefig('output.pdf')
         plt.show()
         return (image)
-    def clicked(n, neighbourhood, r, output_file, custom_x, custom_y, inclusion_number, radius_min, radius_max):
+
+    def clicked(n, neighbourhood, r, output_file, custom_x, custom_y, inclusion_number, radius_min, radius_max, proc):
         custom = dict()
         custom_x = custom_x.get().split(" ")
         custom_y = custom_y.get().split(" ")
@@ -154,23 +153,30 @@ def gui_function():
             radius_max = int(radius_max.get())
         else:
             radius_max = 4
-        image = main(size, custom, random, neighbourhood.get(), output_file.get(), base, inclusion_number, radius_min,
-                     radius_max)
 
-    def callback(x, plt):
+        if proc.get() != "":
+            proc = int(proc.get())
+        else:
+            proc = 40
+        image = main(size, custom, random, neighbourhood.get(), output_file.get(), base, inclusion_number, radius_min,
+                     radius_max, proc)
+
+    def callback(proc_end, plt):
        buf = io.BytesIO()
        plt.savefig(buf, format='png')
        buf.seek(0)
        img = Image.open(buf)
-       img = img.resize((400, 400), Image.ANTIALIAS)
+       img = img.resize((500, 500), Image.ANTIALIAS)
        img = ImageTk.PhotoImage(img)
        panel = Label(window, image=img)
        panel.image = img
-       panel.grid(column=0, row=7)
+       panel.grid(column=0, row=9)
+       panel2 = Label(window, text=proc_end)
+       panel2.grid(column=2, row=9)
 
     window = Tk()
     window.title("Welcome to LikeGeeks app")
-    window.geometry('680x560')
+    window.geometry('780x660')
 
     lbl = Label(window, text="the size of the square")
     lbl.grid(column=0, row=0)
@@ -178,53 +184,57 @@ def gui_function():
     lbl = Label(window, text="type of Neighbourhood")
     lbl.grid(column=0, row=1)
 
-    lbl = Label(window, text="Number of random start place")
+    lbl = Label(window, text="% for Grain_Curvature")
     lbl.grid(column=0, row=2)
 
-    lbl = Label(window, text="Type of output file")
+    lbl = Label(window, text="Number of random start place")
     lbl.grid(column=0, row=3)
 
-    lbl = Label(window, text="parametr of custom start point: X and Y")
+    lbl = Label(window, text="Type of output file")
     lbl.grid(column=0, row=4)
 
-    lbl = Label(window, text="Number of random inclusions point and radius min-max")
+    lbl = Label(window, text="parametr of custom start point: X and Y")
     lbl.grid(column=0, row=5)
+
+    lbl = Label(window, text="Number of random inclusions point and radius min-max")
+    lbl.grid(column=0, row=6)
 
     n = Entry(window, width=10)
     n.grid(column=1, row=0)
 
+    proc = Entry(window, width=10)
+    proc.grid(column=1, row=2)
+
     r = Entry(window, width=10)
-    r.grid(column=1, row=2)
+    r.grid(column=1, row=3)
 
     custom_x = Entry(window, width=10)
-    custom_x.grid(column=1, row=4)
+    custom_x.grid(column=1, row=5)
 
     custom_y = Entry(window, width=10)
-    custom_y.grid(column=2, row=4)
+    custom_y.grid(column=2, row=5)
 
     inclusion_number = Entry(window, width=10)
-    inclusion_number.grid(column=1, row=5)
+    inclusion_number.grid(column=1, row=6)
 
     radius_min = Entry(window, width=10)
-    radius_min.grid(column=2, row=5)
+    radius_min.grid(column=2, row=6)
 
     radius_max = Entry(window, width=10)
-    radius_max.grid(column=3, row=5)
+    radius_max.grid(column=3, row=6)
 
     neighbourhood = Combobox(window)
-    neighbourhood['values'] = ("von Neumanna", "Moore’a")
+    neighbourhood['values'] = ("von Neumanna", "Moore’a", "Grain_Curvature")
     neighbourhood.grid(column=1, row=1)
 
     output_file = Combobox(window)
     output_file['values'] = ("non output file", "pdf", "png")
-    output_file.grid(column=1, row=3)
+    output_file.grid(column=1, row=4)
 
-    btn = Button(window, text="Add data", command=lambda: clicked(n, neighbourhood, r, output_file, custom_x, custom_y, inclusion_number, radius_max, radius_min))
-    btn.grid(column=0, row=6)
-
+    btn = Button(window, text="Add data", command=lambda: clicked(n, neighbourhood, r, output_file, custom_x, custom_y, inclusion_number, radius_max, radius_min, proc))
+    btn.grid(column=0, row=8)
 
     window.mainloop()
-
 
 
 def value_decide(list_value):
@@ -317,6 +327,53 @@ def one_grow(board, base, n, check_new, neighbourhood):
                         board2[i][j] = decided_value
                 new_value.clear()
     return board2
+
+
+def grain_curvature(board, base, n, check_new, chance):
+    board2 = copy.deepcopy(board)
+    rule2_list = list()
+    rule3_list = list()
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] not in check_new and board[i][j] != stop_point:
+                if i != (n-1) and board[i+1][j] != base and board[i+1][j] != stop_point:
+                    rule2_list.append(board[i+1][j])
+                if j != (n-1) and board[i][j+1] != base and board[i][j+1] != stop_point:
+                    rule2_list.append(board[i][j+1])
+                if j != 0 and board[i][j-1] != base and board[i][j-1] != stop_point:
+                    rule2_list.append(board[i][j-1])
+                if i != 0 and board[i-1][j] != base and board[i-1][j] != stop_point:
+                    rule2_list.append(board[i-1][j])
+                if i != (n - 1) and j != 0 and board[i + 1][j-1] != base and board[i + 1][j-1] != stop_point:
+                    rule3_list.append(board[i + 1][j-1])
+                if j != (n - 1) and i != (n - 1) and board[i+1][j + 1] != base and board[i+1][j + 1] != stop_point:
+                    rule3_list.append(board[i+1][j + 1])
+                if i != 0 and j != (n - 1) and board[i-1][j + 1] != base and board[i-1][j + 1] != stop_point:
+                    rule3_list.append(board[i-1][j + 1])
+                if i != 0 and j != 0 and board[i - 1][j-1] != base and board[i - 1][j-1] != stop_point:
+                    rule3_list.append(board[i - 1][j-1])
+                if len(rule2_list + rule3_list) != 0:
+                    for k in set(rule2_list + rule3_list):
+                        if (rule2_list + rule3_list).count(k) >= 5:
+                            board2[i][j] = k
+                if len(rule2_list) != 0:
+                    for k in set(rule2_list):
+                        if rule2_list.count(k) >= 3:
+                            board2[i][j] = k
+
+                if len(rule3_list) != 0:
+                    for k in set(rule3_list):
+                        if rule3_list.count(k) >= 3:
+                            board2[i][j] = k
+                if len(rule2_list + rule3_list) != 0:
+                    decided_value = value_decide(rule2_list + rule3_list)
+                    if decided_value != stop_point:
+                        if random.randint(0, 100) <= chance:
+                            board2[i][j] = decided_value
+                rule2_list.clear()
+                rule3_list.clear()
+    return board2
+
 
 if __name__ == '__main__':
     args = parse_arguments()
